4904ea9659c65ef120a70eb0c6ab6164ca77458b
rhit-daviscl <daviscl@rose-hulman.edu>
2024-02-15 22:51:31 -0500

    added create init functionality

--- user/threads.c
+++ user/threads.c
@@ -1,4 +1,5 @@
 #include "kernel/types.h"
+#include "kernel/proc.c"
 #include "user.h"
 
 
@@ -9,9 +10,12 @@ struct thread_t {
 };
 
 
-int thread_create(struct thread_t *thread, void (*f)(void*), void* arg) {
-
-    return 0;
+int thread_create(struct thread_t *thread, void(*f)(void*), void* arg) {
+    void* pagetable = malloc(PGSIZE*2);
+    if(pagetable == 0) panic("The malloc did not malloc any memory!");
+    if(*(uint*)pagetable % PGSIZE == 0) thread->stack = pagetable;
+    else thread->stack = pagetable - (*(uint*)pagetable % PGSIZE);
+    return procclone(f, arg, thread->stack);
 }
 
 int thread_join(struct thread_t thread) {
