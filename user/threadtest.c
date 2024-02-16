9d3019408c19799fe20eaa94c5f86f2ad3981b10
rhit-millereb <millereb@rose-hulman.edu>
2024-02-15 21:14:23 -0500

    Adding first steps to testing the thread

--- user/threadtest.c
+++ user/threadtest.c
@@ -1,9 +1,24 @@
 #include "kernel/types.h"
 #include "user.h"
 #include "kernel/stat.h"
+#include "threads.c"
+
+
+void test_thread_create(void *arg) {
+    int *id = ((int*) arg);
+
+    printf("Thread Started Function with ID: %d", *id);
+
+}
 
 
 void main(void) {
 
     printf("Testing Threads: \n");
+
+    printf("\nCreating a thread:\n");
+    
+    struct thread_t thread;
+
+    thread_create(&thread, *test_thread_create, (void*) 1);
 }
